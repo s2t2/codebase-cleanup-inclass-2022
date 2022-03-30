eb01e89fe68c6899ed969371bfbdf85400929bb9
MJ Rossetti <s2t2@users.noreply.github.com>
2022-03-30 16:29:00 -0400

    Integration test for network request

--- app/unemployment.py
+++ app/unemployment.py
@@ -12,59 +12,75 @@ load_dotenv()
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
-# docs: https://www.alphavantage.co/documentation/#unemployment
-url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={ALPHAVANTAGE_API_KEY}"
-response = requests.get(url)
-parsed_response = json.loads(response.text)
-#print(parsed_response)
 
-data = parsed_response["data"]
-latest = data[0]
-print(latest) #> {'date': '2022-02-01', 'value': '3.8'}
+def fetch_unemployment_data():
+    url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={ALPHAVANTAGE_API_KEY}"
+    response = requests.get(url)
+    parsed_response = json.loads(response.text)
+    #print(parsed_response)
+    return parsed_response["data"]
 
 
-#exit()
 
-#
-# DATA AND CHARTING
-#
-
-from pandas import DataFrame
-from plotly.express import bar
+if __name__ == "__main__":
 
 
-df = DataFrame(data)
-print(df.head())
+    ## docs: https://www.alphavantage.co/documentation/#unemployment
+    #url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={ALPHAVANTAGE_API_KEY}"
+    #response = requests.get(url)
+    #parsed_response = json.loads(response.text)
+    ##print(parsed_response)
+    #data = parsed_response["data"]
 
-fig = bar(df, x="date", y="value", title="Unemployment Rates")
-# https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
+    # refactored (so we can test this function)
+    data = fetch_unemployment_data()
 
-# https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.update_yaxes
-# https://plotly.com/python/reference/layout/yaxis/
-# https://plotly.com/python/reference/layout/yaxis/#layout-yaxis-ticksuffix
-fig.update_yaxes(
-    #tickprefix="$",
-    ticksuffix="%",
-    showgrid=True
-)
-
-fig.show()
-
-#breakpoint()
+    latest = data[0]
+    print(latest) #> {'date': '2022-02-01', 'value': '3.8'}
 
 
-print("DATAVIZ EXPORT...")
-# https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.to_image
-# fig.to_image(format="png")
+    #exit()
 
-# https://plotly.com/python/static-image-export/
-# Image export using the "kaleido" engine requires the kaleido package,
-#which can be installed using pip:
-#    $ pip install -U kaleido
-img_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.png")
-fig.write_image(img_filepath)
+    #
+    # DATA AND CHARTING
+    #
+
+    from pandas import DataFrame
+    from plotly.express import bar
 
 
-print("CSV EXPORT...")
-csv_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.csv")
-df.to_csv(csv_filepath, index=False)
+    df = DataFrame(data)
+    print(df.head())
+
+    fig = bar(df, x="date", y="value", title="Unemployment Rates")
+    # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
+
+    # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.update_yaxes
+    # https://plotly.com/python/reference/layout/yaxis/
+    # https://plotly.com/python/reference/layout/yaxis/#layout-yaxis-ticksuffix
+    fig.update_yaxes(
+        #tickprefix="$",
+        ticksuffix="%",
+        showgrid=True
+    )
+
+    fig.show()
+
+    #breakpoint()
+
+
+    print("DATAVIZ EXPORT...")
+    # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.to_image
+    # fig.to_image(format="png")
+
+    # https://plotly.com/python/static-image-export/
+    # Image export using the "kaleido" engine requires the kaleido package,
+    #which can be installed using pip:
+    #    $ pip install -U kaleido
+    img_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.png")
+    fig.write_image(img_filepath)
+
+
+    print("CSV EXPORT...")
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.csv")
+    df.to_csv(csv_filepath, index=False)
